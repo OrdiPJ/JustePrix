73c77dfb2e16aa0cdcfc374f900534c57fc64094
Paulin J <paulin.juhen@outlook.com>
2022-03-05 19:14:28 +0100

    Ajout d'un titre pour la fenetre de console + Niveaux de difficulté

--- JustePrix/main.cpp
+++ JustePrix/main.cpp
@@ -5,22 +5,44 @@
 
 int main()
 {
+	// Titre de l'onglet
+	system("TITLE Jeu du Juste Prix");
+
 	// Déclaration des variables :
 	std::string user;
 	int num{ 0 };
 	int userNum{0};
 	int ess{0};
+	int diff{ 0 };
 
 	// Génération d un nombre aleatoire
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	num = std::generate_canonical<double, 10>(gen) * 1024;
 	
 
-	// Affichage du nom de l'utilisateur + but du jeu
+	// Affichage du nom de l'utilisateur + choix de difficulté
 	std::cout << "Entre ton nom : ";
 	std::getline(std::cin, user);
-	std::cout << "Bonjour " << user << " bienvenue au jeu du juste prix !\nTon but est de trouver un nombre generer aleatoirement comprit entre 1 et 1024\n";
+	std::cout << "Bonjour " << user << " bienvenue au jeu du juste prix !\nChoisie une difficulte (1,2,3) : ";
+	std::cin >> diff;
+
+	switch (diff)
+	{
+	case 1:
+		std::cout << "\nTu as choisi la difficulté facile, to but est de trouver le nombre compris entre 0 et 256\n";
+		num = std::generate_canonical<double, 10>(gen) * 256;
+		break;
+	case 2:
+		std::cout << "\nTu as choisi la difficulté moyenne, to but est de trouver le nombre compris entre 0 et 512\n";
+		num = std::generate_canonical<double, 10>(gen) * 512;
+		break;
+	case 3:
+		std::cout << "\nTu as choisi la difficulté difficile, to but est de trouver le nombre compris entre 0 et 1024\n";
+		num = std::generate_canonical<double, 10>(gen) * 1024;
+		break;
+	default:
+		break;
+	}
 	
 	// Boucle tant que l'utilisateur n'a pas entrer le bon nombre
 	while (userNum != num)
